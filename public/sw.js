c2358f9bf03ec156e82541909e47de0a4f2ebdba
Elkah07 <Kathie.lopes.pro@gmail.com>
2026-07-31 16:53:17 +0200

    GROSSE MAL

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "dernier-neurone-v1";
-const SHELL = ["/", "/manifest.webmanifest", "/app-icon.png", "/logo-full.png"];
+const CACHE = "dernier-neurone-v2";
+const SHELL = ["/", "/manifest.webmanifest", "/app-icon-192.png", "/app-icon-512.png", "/logo-full.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", event => {
